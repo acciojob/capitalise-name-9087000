735bc3f5a20477eef105d5eb8a1d666e66e22d24
9087000 <50361103+9087000@users.noreply.github.com>
2024-10-12 18:29:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function convertToUppercase() {
+    // Get the input field
+    const inputField = document.getElementById("fname");
+    
+    // Convert the value to uppercase
+    inputField.value = inputField.value.toUpperCase();
+}
